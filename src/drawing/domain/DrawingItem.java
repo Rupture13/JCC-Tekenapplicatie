c3694b48ff44f68e3b57203c9dc0077497e59e69
gebruiker <gebruiker@192.168.0.104>
2017-10-04 05:17:03 +0200

    • Finished assignment 4.

--- src/drawing/domain/DrawingItem.java
+++ src/drawing/domain/DrawingItem.java
@@ -47,6 +47,35 @@ public abstract class DrawingItem implements Serializable {
     
     public abstract void paintUsing(IPaintable paintable);
     
+    public boolean overlaps(DrawingItem item) {
+        boolean result = false;
+        if (item.insideBoundingBox(this.getAnchor())) {
+            result = true;
+        }
+        else if (item.insideBoundingBox(new Point((this.getAnchor().getX() + this.getWidth()), this.getAnchor().getY()))) {
+            result = true;
+        }
+        else if (item.insideBoundingBox(new Point(this.getAnchor().getX(), (this.getAnchor().getY() + this.getHeight())))) {
+            result = true;
+        }
+        else if (item.insideBoundingBox(new Point((this.getAnchor().getX() + this.getWidth()), (this.getAnchor().getY() + this.getHeight())))) {
+            result = true;
+        }
+        return result;
+    }
+    
+    public boolean insideBoundingBox(Point point) {
+        boolean result = false;
+        if (point.getX() >= this.getAnchor().getX() 
+            && point.getX() <= (this.getAnchor().getX() + this.getWidth())
+            && point.getY() >= this.getAnchor().getY()
+            && point.getY() <= (this.getAnchor().getY() + this.getHeight())) 
+        {
+            result = true;
+        }
+        return result;
+    }
+    
     @Override
     public abstract String toString();
 }
